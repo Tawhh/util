c8b693182d4698d00d8b7b513e8e04902f248e12
魏洪皓 <916952748@qq.com>
2017-04-12 16:57:25 +0800

    BeanCopy工具类

--- BeanCopy.java
+++ BeanCopy.java
@@ -45,45 +45,4 @@ public class BeanCopierMapper {
 
     }
 
-/**
-	 * 百分比计算
-	 * @param y
-	 * @param z
-     * @return
-     */
-	public static String myPercent(int y, int z) {
-		double yy=y*1.0;
-		double xx=z*1.0;
-		double percent = 0 ;
-		if (xx!=0){
-			 percent = yy / xx;
-		}
-		NumberFormat nt = NumberFormat.getPercentInstance();
-		nt.setMinimumFractionDigits(2);
-		return nt.format(percent);
-	}
-
-/**
-	 * 转ajax请求json数据
-	 * @param jsonOb
-	 * @param response
-     */
-	public static void printJsonForJSONP(JSONObject jsonOb, HttpServletResponse response)
-	{
-		response.setCharacterEncoding("utf-8");
-		response.setContentType("text/json; charset=utf-8");
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
-		try
-		{
-			response.getWriter().write(JSON.toJSONString(jsonOb, new SerializerFeature[] { SerializerFeature.WriteMapNullValue, SerializerFeature.WriteNullStringAsEmpty, SerializerFeature.WriteNullListAsEmpty, SerializerFeature.WriteNullNumberAsZero, SerializerFeature.WriteNullBooleanAsFalse }));
-			response.getWriter().flush();
-			response.getWriter().close();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
 }
